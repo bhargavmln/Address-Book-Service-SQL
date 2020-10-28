fd3a3144db0620046441059280913ffcf26e17ca
bhargavmln <bhargavmln@gmail.com>
2020-10-28 21:18:47 +0530

    [bhargavmln]create: created address book table

--- Address Book Service.sql
+++ Address Book Service.sql
@@ -3,3 +3,17 @@ CREATE DATABASE address_book_service;
 SHOW databases;
 USE address_book_service;
 SELECT database();
+
+#UC2
+CREATE TABLE address_book(
+	 firstname varchar(50) NOT NULL,
+	 lastname varchar(50) NOT NULL,
+	 address varchar(250) NOT NULL,
+	 city varchar(100) NOT NULL,
+	 state varchar(100) NOT NULL,
+	 zip int unsigned NOT NULL,
+	 phone int NOT NULL,
+	 email varchar(250) NOT NULL
+ );
+ DESCRIBE address_book;
+ 
