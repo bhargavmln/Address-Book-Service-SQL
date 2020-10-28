cdc3fabe0923372d932ebbebf891dfa9ffd88b7f
bhargavmln <bhargavmln@gmail.com>
2020-10-28 21:47:36 +0530

    [bhargavmln]insert: insert new contacts to address book

--- Address Book Service.sql
+++ Address Book Service.sql
@@ -11,9 +11,17 @@ CREATE TABLE address_book(
 	 address varchar(250) NOT NULL,
 	 city varchar(100) NOT NULL,
 	 state varchar(100) NOT NULL,
-	 zip int unsigned NOT NULL,
-	 phone int NOT NULL,
+	 zip varchar(6) NOT NULL,
+	 phone varchar(12) NOT NULL,
 	 email varchar(250) NOT NULL
  );
  DESCRIBE address_book;
+ 
+ #UC3
+ INSERT INTO address_book(firstname,lastname,address,city,state,zip,phone,email) VALUES
+ ('Naruto','Uzumaki','Konoha','Leaf  Village','Shippuden',936363,999999999,'naruto@gmail.com'),
+ ('Sasuke','Uchiha','Konoha','Leaf  Village','Shippuden',936369,888888888,'uchiha@gmail.com'),
+ ('Hinata','Hyuga','Konoha','Leaf  Village','Shippuden',565656,7777777777,'Hinata@gmail.com');
+ SELECT * FROM address_book;
+ 
  
